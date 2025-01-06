e9bdd986ae7a83c55d225dd5ddb82d5c42c7ef30
romeelk <romeel_k@yahoo.co.uk>
2025-01-06 15:13:35 +0000

    Added Chromadb example

--- src/embedding_plot/main.py
+++ src/embedding_plot/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Embedding dimensions: Formaility, Negativity , Activity
+# Embedding dimensions: Formaility, Negativity , Activity (three dimensions)
 words = ["Hello", "Shutup", "Go away", "Run", "Walk"]
 
 # Embedding dimensions
